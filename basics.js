05890d2947ba9173b3148736c5e15909dbad8bce
Aakash Kumar Nanda <aakash.nanda99@gmail.com>
2023-01-21 19:00:29 +0530

    Create basics.js

---
+++ basics.js
@@ -0,0 +1,53 @@
+
+
+console.log("Hello World!!");
+
+//var, let, const
+
+var x = 10
+x = 20
+console.log(x)
+
+const y = 100
+console.log(y)
+
+let z = 200
+z = 150
+console.log(z)
+
+const age = 17
+
+if(age>=18){
+    console.log("You are eligible to vote")
+}else{
+    console.log("You are not eligible to vote")
+}
+
+let i = 1
+for(i = 1;i<=10;i++){
+    console.log(i*5)
+}
+
+
+function display(){
+    console.log("I am a function")
+}
+
+display()
+display()
+
+function add(x, y){
+    const sum = x+y
+    console.log(sum)
+}
+
+add(20,30)
+
+
+
+
+
+
+
+
+
