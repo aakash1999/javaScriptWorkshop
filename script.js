e39bb3098a77c3ea94bb62383ba072b98fd79cc8
Aakash Kumar Nanda <aakash.nanda99@gmail.com>
2023-01-21 19:00:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function displayMessage(){
+    document.getElementById("hiddenMessage").style.display = 'block'
+}
+
+function countDown(){
+    let currentVal = document.getElementById("countDownButton").innerHTML
+    
+    let newVal = 0
+    
+    if(currentVal > 0){
+        newVal = currentVal - 1
+    }
+    
+    document.getElementById("countDownButton").innerHTML = newVal
+}
